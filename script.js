b91fd783503edb416f912889d260b3d62c7a0d50
yookawa@dl-techx-xedge.net <yookawa@dl-techx-xedge.net>
2025-10-24 13:50:39 +0900

    資源のデプロイ

---
+++ script.js
@@ -0,0 +1,45 @@
+const { createApp, ref } = Vue;
+const { createVuetify } = Vuetify;
+
+const vuetify = createVuetify();
+
+createApp({
+  setup() {
+    const ID = ref('');
+    const Name = ref('');
+    const dataList = ref([]);
+
+    const addData = async () => {
+      if (!ID.value || isNaN(ID.value)) {
+        console.log("IDに数値が入力されていません");
+        return;
+      }
+
+      const param = {
+        ID: ID.value,
+        Name: Name.value,
+      };
+
+      // 実際のAPI URLに置き換えてください
+      const response = await axios.post('https://m3h-ookawa-containerapp.ashywater-59bed562.japaneast.azurecontainerapps.io/api/INSERT', param);
+      console.log(response.data);
+    };
+
+    const readData = async () => {
+      // 実際のAPI URLに置き換えてください
+      const response = await axios.get('https://m3h-ookawa-containerapp.ashywater-59bed562.japaneast.azurecontainerapps.io/api/SELECT');
+      console.log(response.data);
+      dataList.value = response.data.List;
+    };
+
+    return {
+      ID,
+      Name,
+      dataList,
+      addData,
+      readData,
+    };
+  },
+})
+  .use(vuetify)
+  .mount('#app');
